b9ca41f85b5de073beff7e184bf3db90dd686681
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-08 15:12:58 -0300

    Add project files.

---
+++ Exercicio_Matriz01/Program.cs
@@ -0,0 +1,62 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] mat = new int[3,3];
+
+        for(int l=0; l < 3; l++)
+        {
+            for(int c=0; c < 3; c++)
+            {
+                mat[l,c] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        //impressao da primeira coluna
+
+        for (int l = 0; l < 3; l++)
+        {
+            Console.WriteLine(mat[l, 0]);
+        }
+
+        //impressao de todas as lihas de cada coluna
+
+
+        for (int l = 0; l < 3; l++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                Console.Write(mat[l, c] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        //diagonal principal 
+
+        for (int l = 0; l < 3; l++)
+        {
+            Console.WriteLine(mat[l, l]);
+        }
+
+        //diagonal segundaria
+
+        for (int l = 0; l < 3; l++)
+        {
+            for(int c = 2; c >= 0; c--)
+            {
+                Console.WriteLine(mat[l,c]);
+            }
+        }
+
+        //teste
+
+        for (int l = 0; l < 3; l++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                Console.Write(mat[l,c]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
